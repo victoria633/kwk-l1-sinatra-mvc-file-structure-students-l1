2da4e6de6762a384ee43f71b95995ad0bc5f2e32
victoria633 <victoria6411@hotmail.com>
2018-06-13 21:06:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,16 @@ class ApplicationController < Sinatra::Base
   	set :public_dir, "public"
   end
 
-  get "/" do
+  get '/' do
   	erb :index
   end
+  
+  post '/' do
+  @the_fortune=get_fortune
+  @the_user=params[:user]
+  return erb :results
+end
+  
+  
+  
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,8 @@
-class Model
-  # Replace with Dog clas
+
+def get_fortune()
+  #method gose here 
+  fortunes_array = ["You'll have a great day","Someone's going into the bathroom in the next 20 min","You'll have sandwiches for the rest of your life", "Jessica said no","when a ball is thrown at you, you will dive into the sand", "you will win in mafia","you will be the mafia"]
+  return fortunes_array.sample
 end
+
+#puts get_fortune
